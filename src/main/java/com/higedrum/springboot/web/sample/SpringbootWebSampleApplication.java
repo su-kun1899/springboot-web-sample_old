79c747d9cb0ea22886317bc87315cbff9b5aafe4
Koji Sudo <higedrum.coz@gmail.com>
2016-06-29 21:10:49 +0900

    initial commit

---
+++ src/main/java/com/higedrum/springboot/web/sample/SpringbootWebSampleApplication.java
@@ -0,0 +1,12 @@
+package com.higedrum.springboot.web.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebSampleApplication.class, args);
+	}
+}
