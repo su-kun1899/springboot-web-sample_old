3e87540f0fe5d2525d0106d000f9ede09d98197c
su-kun1899 <higedrum.coz@gmail.com>
2016-07-13 00:31:32 +0900

    不要なフィルタを削除

--- src/main/java/com/higedrum/springboot/web/sample/AppConfig.java
+++ src/main/java/com/higedrum/springboot/web/sample/AppConfig.java
@@ -1,13 +1,18 @@
 package com.higedrum.springboot.web.sample;
 
 import net.sf.log4jdbc.Log4jdbcProxyDataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CharacterEditor;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.sql.DataSource;
 
@@ -18,22 +23,22 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class AppConfig {
-    @Autowired
-    DataSourceProperties dataSourceProperties;
-    DataSource dataSource;
+  @Autowired
+  DataSourceProperties dataSourceProperties;
+  DataSource dataSource;
 
-    @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
-    @Bean(destroyMethod = "close")
-    DataSource realDataSource() {
-        DataSourceBuilder factory = DataSourceBuilder.create(this.dataSourceProperties.getClassLoader()).url(this.dataSourceProperties.getUrl()).username(this.dataSourceProperties.getUsername()).password(this.dataSourceProperties.getPassword());
-        this.dataSource = factory.build();
-        return this.dataSource;
-    }
+  @ConfigurationProperties(prefix = DataSourceProperties.PREFIX)
+  @Bean(destroyMethod = "close")
+  DataSource realDataSource() {
+    DataSourceBuilder factory = DataSourceBuilder.create(this.dataSourceProperties.getClassLoader()).url(this.dataSourceProperties.getUrl()).username(this.dataSourceProperties.getUsername()).password(this.dataSourceProperties.getPassword());
+    this.dataSource = factory.build();
+    return this.dataSource;
+  }
 
-    @Primary
-    @Bean
-    DataSource dataSource() {
-        return new Log4jdbcProxyDataSource(this.dataSource);
-    }
+  @Primary
+  @Bean
+  DataSource dataSource() {
+    return new Log4jdbcProxyDataSource(this.dataSource);
+  }
 
 }
